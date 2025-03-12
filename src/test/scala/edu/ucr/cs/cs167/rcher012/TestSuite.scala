5777722426e49fd7785b60eb424bccea5f683fa2
Rahul  Cherkuri <rahulcherkuri@rahuls-mbp.lan>
2025-03-12 09:30:00 -0700

    basic test framework
    placeholder for now
    need to add actual test cases
    maven test setup

---
+++ src/test/scala/edu/ucr/cs/cs167/rcher012/TestSuite.scala
@@ -0,0 +1,9 @@
+package edu.ucr.cs.cs167.rcher012
+
+// Test framework placeholder
+object TestSuite {
+  def runTests(): Unit = {
+    println("Running wildfire analysis tests...")
+    // Test implementations to follow
+  }
+}
